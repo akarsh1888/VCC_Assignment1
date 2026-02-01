607776a426fbaa21c251649fe122821472536a1c
Akarrsh <akarsh1888@gmail.com>
2026-02-01 21:48:01 +0530

    Create client.js

---
+++ client.js
@@ -0,0 +1,13 @@
+const axios = require("axios");
+
+axios.post("http://192.168.56.101:3000/save", {
+  name: "Akarsh",
+  project: "Virtual Box Assignment",
+  from: "Client-VM"
+})
+.then(res => {
+  console.log(res.data);
+})
+.catch(err => {
+  console.error(err.message);
+});
